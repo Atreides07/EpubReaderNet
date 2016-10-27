c5d004daa106d61d171df3eb438af09d0e88a0e0
Sheriev Akhmed <sherievakhmed@live.ru>
2016-10-27 20:16:26 +0300

    EpubReader

---
+++ EpubReader.Test/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace EpubReader.Test
+{
+    [TestFixture]
+    public class EpubReader31Test
+    {
+        [Test]
+        public async Task Epub()
+        {
+            using (var fs = File.Open("TestContent/Epub31/moby-dick-31-html.epub", FileMode.Open,FileAccess.Read,FileShare.Read))
+            {
+                var epubReader = new EpubReader();
+                var epub=epubReader.Reader(fs);
+            }
+        }
+
+        
+    }
+}
---
+++ EpubReader/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace EpubReader
+{
+    public class EpubReader
+    {
+        public EpubContent Reader(Stream stream)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class EpubContent
+    {
+    }
+}

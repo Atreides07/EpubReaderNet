a4d87b9689349969a6a7f7c144eec8b48d969a2c
Sheriev Akhmed <sherievakhmed@live.ru>
2016-10-28 22:27:37 +0300

    change test

---
+++ EpubReader.Test/EpubReader31Test.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Reflection;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace EpubReader.Test
+{
+    [TestFixture]
+    public class EpubReader31Test
+    {
+        string projectDir;
+        [SetUp]
+        public void Setup()
+        {
+            projectDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        }
+
+        [Test]
+        public async Task Epub()
+        {
+            using (var fs = GetFile("TestContent/Epub31/moby-dick-31-html.epub"))
+            {
+                var epubReader = new EpubReader();
+                epubReader.Reader(fs);
+            }
+        }
+
+        private Stream GetFile(string fileName)
+        {
+            return File.Open(Path.Combine(projectDir, fileName), FileMode.Open, FileAccess.Read, FileShare.Read);
+        }
+    }
+}
